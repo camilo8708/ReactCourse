3bba040fc3d4c0062a82664161bfd4b4343a953e
Camilo <camilo8708@gmail.com>
2019-06-11 21:56:02 -0500

    Basico

--- basicosreact/src/App.js
+++ basicosreact/src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+  import React, { Fragment } from 'react';
+  import PrimerComponente from './components/PrimerComponente'
 
-class App extends Component {
-  render() {
+  function App() {
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Fragment>
+        <PrimerComponente />
+        <PrimerComponente />
+        <PrimerComponente />
+        <PrimerComponente />
+      </Fragment>
     );
   }
-}
 
-export default App;
+  export default App;
